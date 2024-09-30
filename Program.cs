af2cd004ac03a0d213565ea84bb441415d14aeba
Duc.NT <311020ntd@gmail>
2024-09-30 15:22:10 +0700

    update

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string readFile = @"C:\Users\ASUS\Desktop\New folder\read_file_vcs\read.csv";
+using (StreamReader streamReader = new StreamReader(readFile)){
+    string line ="";
+    while ((line = streamReader.ReadLine())!=null)
+    {
+        Console.WriteLine(line);
+
+    }
+
+}
